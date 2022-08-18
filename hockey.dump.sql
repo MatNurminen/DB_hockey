e610b2470658f9c08fbdc632d5edbedfea2a5ed5
Matvey Nurminen <mat.nurminen@hotmail.com>
2022-08-18 17:34:46 +0300

    new schema db for tournaments

--- hockey.dump.sql
+++ hockey.dump.sql
@@ -21,101 +21,19 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: championships_by_clubs; Type: TABLE; Schema: public; Owner: gb_user
+-- Name: team_logos; Type: TABLE; Schema: public; Owner: gb_user
 --
 
-CREATE TABLE public.championships_by_clubs (
+CREATE TABLE public.team_logos (
     id integer NOT NULL,
-    year smallint NOT NULL,
-    club_id integer NOT NULL,
-    games smallint,
-    wings smallint,
-    ties smallint,
-    losts smallint,
-    goals_for smallint,
-    goals_against smallint,
-    postseason character varying(50)
-);
-
-
-ALTER TABLE public.championships_by_clubs OWNER TO gb_user;
-
---
--- Name: championships_by_clubs_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
---
-
-CREATE SEQUENCE public.championships_by_clubs_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.championships_by_clubs_id_seq OWNER TO gb_user;
-
---
--- Name: championships_by_clubs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
---
-
-ALTER SEQUENCE public.championships_by_clubs_id_seq OWNED BY public.championships_by_clubs.id;
-
-
---
--- Name: championships_by_players; Type: TABLE; Schema: public; Owner: gb_user
---
-
-CREATE TABLE public.championships_by_players (
-    id integer NOT NULL,
-    year smallint NOT NULL,
-    player_id integer NOT NULL,
-    club_id integer NOT NULL,
-    games smallint,
-    wings smallint,
-    goals smallint,
-    postseason character varying(50)
-);
-
-
-ALTER TABLE public.championships_by_players OWNER TO gb_user;
-
---
--- Name: championships_by_players_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
---
-
-CREATE SEQUENCE public.championships_by_players_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.championships_by_players_id_seq OWNER TO gb_user;
-
---
--- Name: championships_by_players_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
---
-
-ALTER SEQUENCE public.championships_by_players_id_seq OWNED BY public.championships_by_players.id;
-
-
---
--- Name: club_logos; Type: TABLE; Schema: public; Owner: gb_user
---
-
-CREATE TABLE public.club_logos (
-    id integer NOT NULL,
-    club_id integer NOT NULL,
+    team_id integer NOT NULL,
     start_year smallint NOT NULL,
     end_year smallint,
     logo character varying(120) NOT NULL
 );
 
 
-ALTER TABLE public.club_logos OWNER TO gb_user;
+ALTER TABLE public.team_logos OWNER TO gb_user;
 
 --
 -- Name: club_logos_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
@@ -136,22 +54,23 @@ ALTER TABLE public.club_logos_id_seq OWNER TO gb_user;
 -- Name: club_logos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
 --
 
-ALTER SEQUENCE public.club_logos_id_seq OWNED BY public.club_logos.id;
+ALTER SEQUENCE public.club_logos_id_seq OWNED BY public.team_logos.id;
 
 
 --
--- Name: clubs; Type: TABLE; Schema: public; Owner: gb_user
+-- Name: teams; Type: TABLE; Schema: public; Owner: gb_user
 --
 
-CREATE TABLE public.clubs (
+CREATE TABLE public.teams (
     id integer NOT NULL,
     full_name character varying(120) NOT NULL,
     name character varying(50) NOT NULL,
-    short_name character varying(5) NOT NULL
+    short_name character varying(5) NOT NULL,
+    nation_id integer
 );
 
 
-ALTER TABLE public.clubs OWNER TO gb_user;
+ALTER TABLE public.teams OWNER TO gb_user;
 
 --
 -- Name: clubs_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
@@ -172,14 +91,14 @@ ALTER TABLE public.clubs_id_seq OWNER TO gb_user;
 -- Name: clubs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
 --
 
-ALTER SEQUENCE public.clubs_id_seq OWNED BY public.clubs.id;
+ALTER SEQUENCE public.clubs_id_seq OWNED BY public.teams.id;
 
 
 --
--- Name: countries; Type: TABLE; Schema: public; Owner: gb_user
+-- Name: nations; Type: TABLE; Schema: public; Owner: gb_user
 --
 
-CREATE TABLE public.countries (
+CREATE TABLE public.nations (
     id integer NOT NULL,
     name character varying(50) NOT NULL,
     short_name character varying(5) NOT NULL,
@@ -188,7 +107,7 @@ CREATE TABLE public.countries (
 );
 
 
-ALTER TABLE public.countries OWNER TO gb_user;
+ALTER TABLE public.nations OWNER TO gb_user;
 
 --
 -- Name: countries_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
@@ -209,7 +128,7 @@ ALTER TABLE public.countries_id_seq OWNER TO gb_user;
 -- Name: countries_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
 --
 
-ALTER SEQUENCE public.countries_id_seq OWNED BY public.countries.id;
+ALTER SEQUENCE public.countries_id_seq OWNED BY public.nations.id;
 
 
 --
@@ -259,25 +178,12 @@ CREATE TABLE public.leagues (
     short_name character varying(5) NOT NULL,
     start_year smallint NOT NULL,
     end_year smallint,
-    color character varying(50)
+    color character varying(50),
+    is_local boolean DEFAULT false
 );
 
 
 ALTER TABLE public.leagues OWNER TO gb_user;
-
---
--- Name: leagues_clubs; Type: TABLE; Schema: public; Owner: gb_user
---
-
-CREATE TABLE public.leagues_clubs (
-    league_id integer NOT NULL,
-    club_id integer NOT NULL,
-    start_year smallint NOT NULL,
-    end_year smallint
-);
-
-
-ALTER TABLE public.leagues_clubs OWNER TO gb_user;
 
 --
 -- Name: leagues_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
@@ -312,11 +218,11 @@ CREATE TABLE public.players (
     jersey_number smallint NOT NULL,
     player_position character varying(5) NOT NULL,
     player_order smallint NOT NULL,
-    country_id integer NOT NULL,
+    nation_id integer NOT NULL,
     birth_year smallint NOT NULL,
     height smallint,
     weight smallint,
-    draft_club_id integer,
+    draft_team_id integer,
     start_year smallint NOT NULL,
     end_year smallint
 );
@@ -345,6 +251,21 @@ ALTER TABLE public.players_id_seq OWNER TO gb_user;
 
 ALTER SEQUENCE public.players_id_seq OWNED BY public.players.id;
 
+
+--
+-- Name: players_tournaments; Type: TABLE; Schema: public; Owner: gb_user
+--
+
+CREATE TABLE public.players_tournaments (
+    teams_tournament_id integer NOT NULL,
+    player_id integer NOT NULL,
+    games smallint,
+    goals smallint,
+    postseason character varying(250)
+);
+
+
+ALTER TABLE public.players_tournaments OWNER TO gb_user;
 
 --
 -- Name: seasons; Type: TABLE; Schema: public; Owner: gb_user
@@ -382,38 +303,80 @@ ALTER SEQUENCE public.seasons_id_seq OWNED BY public.seasons.id;
 
 
 --
--- Name: championships_by_clubs id; Type: DEFAULT; Schema: public; Owner: gb_user
+-- Name: teams_tournaments; Type: TABLE; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.championships_by_clubs ALTER COLUMN id SET DEFAULT nextval('public.championships_by_clubs_id_seq'::regclass);
+CREATE TABLE public.teams_tournaments (
+    id integer NOT NULL,
+    tournament_id integer NOT NULL,
+    team_id integer NOT NULL,
+    games smallint,
+    wings smallint,
+    ties smallint,
+    losts smallint,
+    goals_for smallint,
+    goals_against smallint,
+    postseason character varying(250)
+);
+
+
+ALTER TABLE public.teams_tournaments OWNER TO gb_user;
+
+--
+-- Name: teams_tournaments_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
+--
+
+CREATE SEQUENCE public.teams_tournaments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.teams_tournaments_id_seq OWNER TO gb_user;
+
+--
+-- Name: teams_tournaments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
+--
+
+ALTER SEQUENCE public.teams_tournaments_id_seq OWNED BY public.teams_tournaments.id;
 
 
 --
--- Name: championships_by_players id; Type: DEFAULT; Schema: public; Owner: gb_user
+-- Name: tournaments; Type: TABLE; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.championships_by_players ALTER COLUMN id SET DEFAULT nextval('public.championships_by_players_id_seq'::regclass);
+CREATE TABLE public.tournaments (
+    id integer NOT NULL,
+    season_id integer NOT NULL,
+    league_id integer NOT NULL
+);
 
 
---
--- Name: club_logos id; Type: DEFAULT; Schema: public; Owner: gb_user
---
-
-ALTER TABLE ONLY public.club_logos ALTER COLUMN id SET DEFAULT nextval('public.club_logos_id_seq'::regclass);
-
-
---
--- Name: clubs id; Type: DEFAULT; Schema: public; Owner: gb_user
---
-
-ALTER TABLE ONLY public.clubs ALTER COLUMN id SET DEFAULT nextval('public.clubs_id_seq'::regclass);
-
+ALTER TABLE public.tournaments OWNER TO gb_user;
 
 --
--- Name: countries id; Type: DEFAULT; Schema: public; Owner: gb_user
+-- Name: tournaments_id_seq; Type: SEQUENCE; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.countries ALTER COLUMN id SET DEFAULT nextval('public.countries_id_seq'::regclass);
+CREATE SEQUENCE public.tournaments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tournaments_id_seq OWNER TO gb_user;
+
+--
+-- Name: tournaments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gb_user
+--
+
+ALTER SEQUENCE public.tournaments_id_seq OWNED BY public.tournaments.id;
 
 
 --
@@ -431,6 +394,13 @@ ALTER TABLE ONLY public.leagues ALTER COLUMN id SET DEFAULT nextval('public.leag
 
 
 --
+-- Name: nations id; Type: DEFAULT; Schema: public; Owner: gb_user
+--
+
+ALTER TABLE ONLY public.nations ALTER COLUMN id SET DEFAULT nextval('public.countries_id_seq'::regclass);
+
+
+--
 -- Name: players id; Type: DEFAULT; Schema: public; Owner: gb_user
 --
 
@@ -445,43 +415,31 @@ ALTER TABLE ONLY public.seasons ALTER COLUMN id SET DEFAULT nextval('public.seas
 
 
 --
--- Data for Name: championships_by_clubs; Type: TABLE DATA; Schema: public; Owner: gb_user
+-- Name: team_logos id; Type: DEFAULT; Schema: public; Owner: gb_user
 --
 
-COPY public.championships_by_clubs (id, year, club_id, games, wings, ties, losts, goals_for, goals_against, postseason) FROM stdin;
-\.
-
-
---
--- Data for Name: championships_by_players; Type: TABLE DATA; Schema: public; Owner: gb_user
---
-
-COPY public.championships_by_players (id, year, player_id, club_id, games, wings, goals, postseason) FROM stdin;
-\.
+ALTER TABLE ONLY public.team_logos ALTER COLUMN id SET DEFAULT nextval('public.club_logos_id_seq'::regclass);
 
 
 --
--- Data for Name: club_logos; Type: TABLE DATA; Schema: public; Owner: gb_user
+-- Name: teams id; Type: DEFAULT; Schema: public; Owner: gb_user
 --
 
-COPY public.club_logos (id, club_id, start_year, end_year, logo) FROM stdin;
-\.
-
-
---
--- Data for Name: clubs; Type: TABLE DATA; Schema: public; Owner: gb_user
---
-
-COPY public.clubs (id, full_name, name, short_name) FROM stdin;
-\.
+ALTER TABLE ONLY public.teams ALTER COLUMN id SET DEFAULT nextval('public.clubs_id_seq'::regclass);
 
 
 --
--- Data for Name: countries; Type: TABLE DATA; Schema: public; Owner: gb_user
+-- Name: teams_tournaments id; Type: DEFAULT; Schema: public; Owner: gb_user
 --
 
-COPY public.countries (id, name, short_name, flag, logo) FROM stdin;
-\.
+ALTER TABLE ONLY public.teams_tournaments ALTER COLUMN id SET DEFAULT nextval('public.teams_tournaments_id_seq'::regclass);
+
+
+--
+-- Name: tournaments id; Type: DEFAULT; Schema: public; Owner: gb_user
+--
+
+ALTER TABLE ONLY public.tournaments ALTER COLUMN id SET DEFAULT nextval('public.tournaments_id_seq'::regclass);
 
 
 --
@@ -496,15 +454,15 @@ COPY public.league_logos (id, league_id, start_year, end_year, logo) FROM stdin;
 -- Data for Name: leagues; Type: TABLE DATA; Schema: public; Owner: gb_user
 --
 
-COPY public.leagues (id, name, short_name, start_year, end_year, color) FROM stdin;
+COPY public.leagues (id, name, short_name, start_year, end_year, color, is_local) FROM stdin;
 \.
 
 
 --
--- Data for Name: leagues_clubs; Type: TABLE DATA; Schema: public; Owner: gb_user
+-- Data for Name: nations; Type: TABLE DATA; Schema: public; Owner: gb_user
 --
 
-COPY public.leagues_clubs (league_id, club_id, start_year, end_year) FROM stdin;
+COPY public.nations (id, name, short_name, flag, logo) FROM stdin;
 \.
 
 
@@ -512,7 +470,15 @@ COPY public.leagues_clubs (league_id, club_id, start_year, end_year) FROM stdin;
 -- Data for Name: players; Type: TABLE DATA; Schema: public; Owner: gb_user
 --
 
-COPY public.players (id, first_name, last_name, jersey_number, player_position, player_order, country_id, birth_year, height, weight, draft_club_id, start_year, end_year) FROM stdin;
+COPY public.players (id, first_name, last_name, jersey_number, player_position, player_order, nation_id, birth_year, height, weight, draft_team_id, start_year, end_year) FROM stdin;
+\.
+
+
+--
+-- Data for Name: players_tournaments; Type: TABLE DATA; Schema: public; Owner: gb_user
+--
+
+COPY public.players_tournaments (teams_tournament_id, player_id, games, goals, postseason) FROM stdin;
 \.
 
 
@@ -525,17 +491,35 @@ COPY public.seasons (id, name, start_year) FROM stdin;
 
 
 --
--- Name: championships_by_clubs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gb_user
+-- Data for Name: team_logos; Type: TABLE DATA; Schema: public; Owner: gb_user
 --
 
-SELECT pg_catalog.setval('public.championships_by_clubs_id_seq', 1, false);
+COPY public.team_logos (id, team_id, start_year, end_year, logo) FROM stdin;
+\.
 
 
 --
--- Name: championships_by_players_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gb_user
+-- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: gb_user
 --
 
-SELECT pg_catalog.setval('public.championships_by_players_id_seq', 1, false);
+COPY public.teams (id, full_name, name, short_name, nation_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: teams_tournaments; Type: TABLE DATA; Schema: public; Owner: gb_user
+--
+
+COPY public.teams_tournaments (id, tournament_id, team_id, games, wings, ties, losts, goals_for, goals_against, postseason) FROM stdin;
+\.
+
+
+--
+-- Data for Name: tournaments; Type: TABLE DATA; Schema: public; Owner: gb_user
+--
+
+COPY public.tournaments (id, season_id, league_id) FROM stdin;
+\.
 
 
 --
@@ -588,58 +572,56 @@ SELECT pg_catalog.setval('public.seasons_id_seq', 1, false);
 
 
 --
--- Name: championships_by_clubs championships_by_clubs_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: teams_tournaments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.championships_by_clubs
-    ADD CONSTRAINT championships_by_clubs_pkey PRIMARY KEY (id);
-
-
---
--- Name: championships_by_players championships_by_players_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
---
-
-ALTER TABLE ONLY public.championships_by_players
-    ADD CONSTRAINT championships_by_players_pkey PRIMARY KEY (id);
+SELECT pg_catalog.setval('public.teams_tournaments_id_seq', 1, false);
 
 
 --
--- Name: club_logos club_logos_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: tournaments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.club_logos
+SELECT pg_catalog.setval('public.tournaments_id_seq', 1, false);
+
+
+--
+-- Name: team_logos club_logos_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+--
+
+ALTER TABLE ONLY public.team_logos
     ADD CONSTRAINT club_logos_pkey PRIMARY KEY (id);
 
 
 --
--- Name: clubs clubs_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: teams clubs_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.clubs
+ALTER TABLE ONLY public.teams
     ADD CONSTRAINT clubs_pkey PRIMARY KEY (id);
 
 
 --
--- Name: countries countries_name_key; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: nations countries_name_key; Type: CONSTRAINT; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.countries
+ALTER TABLE ONLY public.nations
     ADD CONSTRAINT countries_name_key UNIQUE (name);
 
 
 --
--- Name: countries countries_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: nations countries_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.countries
+ALTER TABLE ONLY public.nations
     ADD CONSTRAINT countries_pkey PRIMARY KEY (id);
 
 
 --
--- Name: countries countries_short_name_key; Type: CONSTRAINT; Schema: public; Owner: gb_user
+-- Name: nations countries_short_name_key; Type: CONSTRAINT; Schema: public; Owner: gb_user
 --
 
-ALTER TABLE ONLY public.countries
+ALTER TABLE ONLY public.nations
     ADD CONSTRAINT countries_short_name_key UNIQUE (short_name);
 
 
@@ -649,14 +631,6 @@ ALTER TABLE ONLY public.countries
 
 ALTER TABLE ONLY public.league_logos
     ADD CONSTRAINT league_logos_pkey PRIMARY KEY (id);
-
-
---
--- Name: leagues_clubs leagues_clubs_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
---
-
-ALTER TABLE ONLY public.leagues_clubs
-    ADD CONSTRAINT leagues_clubs_pkey PRIMARY KEY (league_id, club_id);
 
 
 --
@@ -697,6 +671,22 @@ ALTER TABLE ONLY public.players
 
 ALTER TABLE ONLY public.seasons
     ADD CONSTRAINT seasons_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: teams_tournaments teams_tournaments_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+--
+
+ALTER TABLE ONLY public.teams_tournaments
+    ADD CONSTRAINT teams_tournaments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tournaments tournaments_pkey; Type: CONSTRAINT; Schema: public; Owner: gb_user
+--
+
+ALTER TABLE ONLY public.tournaments
+    ADD CONSTRAINT tournaments_pkey PRIMARY KEY (id);
 
 
 --
