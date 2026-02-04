c2c63d5e86a483a18d63ea32ab1f462092f03a68
Matvey Nurminen <52610639+MatNurminen@users.noreply.github.com>
2026-02-04 14:19:10 +0200

    Add unique constraints to tournaments and teams_tournaments

--- ADD CONSTRAINTS.sql
+++ ADD CONSTRAINTS.sql
@@ -1,2 +1,7 @@
 ALTER TABLE tournaments 
 ADD CONSTRAINT unique_season_id_league_id UNIQUE(season_id, league_id);
+
+ALTER TABLE teams_tournaments 
+ADD CONSTRAINT unique_tournament_id_team_id UNIQUE(tournament_id, team_id);
+
+
