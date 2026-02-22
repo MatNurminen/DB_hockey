a11021237a3b3452f845ab2026db0e70d3863ab4
Matvey Nurminen <52610639+MatNurminen@users.noreply.github.com>
2026-02-22 17:18:39 +0200

    Add new indexes for players and fix existing ones

--- indexes for FK.sql
+++ indexes for FK.sql
@@ -1,4 +1,4 @@
-CREATE INDEX leagues_type_id_fk ON leagues(type_id)
-
-CREATE INDEX teams_nation_id_fk ON teams(nation_id)
-
+CREATE INDEX leagues_type_id_fk ON leagues(type_id);
+CREATE INDEX teams_nation_id_fk ON teams(nation_id);
+CREATE INDEX players_draft_team_id_fk ON players(draft_team_id);
+CREATE INDEX players_nation_id_fk ON players(nation_id);
