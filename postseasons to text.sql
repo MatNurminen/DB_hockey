8c99c16da0783b4f35003351d3e2bf1db5829596
Matvey Nurminen <52610639+MatNurminen@users.noreply.github.com>
2026-02-27 14:27:18 +0200

    Modify postseason column and update players view

--- postseasons to text.sql
+++ postseasons to text.sql
@@ -1,8 +1,28 @@
-ALTER TABLE players_tournaments 
-  ALTER COLUMN postseason TYPE text;
+BEGIN;
+DROP VIEW public.players_all_tournaments;
 
-ALTER TABLE teams_tournaments 
+ALTER TABLE public.players_tournaments 
     ALTER COLUMN postseason TYPE text;
 
-ALTER TABLE teams_tournaments 
+ALTER TABLE public.teams_tournaments 
+    ALTER COLUMN postseason TYPE text;
+
+ALTER TABLE public.teams_tournaments 
     ALTER COLUMN postseason DROP DEFAULT;
+
+CREATE VIEW public.players_all_tournaments AS
+ SELECT players_tournaments.id,
+    teams_tournaments.team_id,
+    players_tournaments.player_id,
+    players_tournaments.games,
+    players_tournaments.goals,
+    players_tournaments.postseason,
+    seasons.id AS season,
+    tournaments.league_id
+   FROM ((((public.players_tournaments
+     JOIN public.teams_tournaments ON ((players_tournaments.teams_tournament_id = teams_tournaments.id)))
+     JOIN public.tournaments ON ((teams_tournaments.tournament_id = tournaments.id)))
+     JOIN public.seasons ON ((tournaments.season_id = seasons.id)))
+     JOIN public.leagues ON ((tournaments.league_id = leagues.id)));
+
+COMMIT;
